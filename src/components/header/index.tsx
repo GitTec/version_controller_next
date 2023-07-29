5e8c3dd3b949a44ae11d79323b3d826e37df704d
Jefferson <jef9296@gmail.com>
2023-07-29 20:49:46 -0300

    Inserido barra de navegacao padrao das paginas

---
+++ src/components/header/index.tsx
@@ -0,0 +1,16 @@
+import { Navbar, Container, Nav } from "react-bootstrap";
+import { GiHorseHead } from "react-icons/gi"
+
+export function Header() {
+    return <Navbar bg="primary" data-bs-theme="dark">
+        <Container>
+            <Navbar.Brand href="/"><GiHorseHead /> Pegasus</Navbar.Brand>
+            <Nav className="me-auto">
+                <Nav.Link href="/">Inicio</Nav.Link>
+                <Nav.Link href="/usuarios">Usuários</Nav.Link>
+                <Nav.Link href="/scripts">Scripts</Nav.Link>
+                <Nav.Link href="/versoes">Versões</Nav.Link>
+            </Nav>
+        </Container>
+    </Navbar >
+}
